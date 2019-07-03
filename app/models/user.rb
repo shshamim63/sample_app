3672b4b8d86314178c3bc2c0a6fc0a94e1063891
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-03 23:29:47 +0600

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                foreign_key: "follower_id",
+                                dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -15,32 +25,32 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
- def self.digest(string)
-   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                 BCrypt::Engine.cost
-   BCrypt::Password.create(string, cost: cost)
- end
+   def self.digest(string)
+     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                   BCrypt::Engine.cost
+     BCrypt::Password.create(string, cost: cost)
+   end
 
- def self.new_token
-   SecureRandom.urlsafe_base64
- end
+   def self.new_token
+     SecureRandom.urlsafe_base64
+   end
 
- def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
- end
+   def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+   end
 
- def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
+   def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
+    end
 
- def forget
-    update_attribute(:remember_digest, nil)
- end
+   def forget
+      update_attribute(:remember_digest, nil)
+   end
 
- # Activates an account.
+   # Activates an account.
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
@@ -67,8 +77,27 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
  private
 
